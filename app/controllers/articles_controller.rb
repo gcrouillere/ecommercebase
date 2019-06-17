3bb022273023b233eca9ab3efd7d27bb9b5b8321
guillaume crouillere <gcrouillere@hotmail.fr>
2019-06-17 14:21:40 +0200

    add I18N in text editor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
 
   def update
     I18n.locale = params[:article][:locale]
-    @article = Article.find(params[:article][:id])
+    @article = Article.find(params[:id])
     if @article.update(article_params)
       respond_to do |format|
         format.html { redirection_for_comment_and_articles }
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:content, :name, :user_id, :id, :article_main_photo, :title)
+    params.require(:article).permit(:content, :name, :user_id, :article_main_photo, :title)
   end
 
   def redirection_for_comment_and_articles
